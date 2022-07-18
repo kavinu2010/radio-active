dca69c31ced96df8e3fef401757be76a3e83e6d5
Kavitha <kavinu2010@gmail.com>
2022-07-18 14:39:07 +0200

    adding client

---
+++ client/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+ 
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+ 
+// cd /path/to/PROJECT1
+// git submodule add ssh://path.to.repo/MEDIA
+// git commit -m "Added Media submodule"
