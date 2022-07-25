ec10f882317b558a30544ab9727cfbf990596f3e
salt-dev <salt-dev@salt-devs-MacBook-Pro-2.local>
2022-07-25 09:59:38 +0200

    writing build script and changing port

--- server/index.js
+++ server/index.js
@@ -1,22 +1,23 @@
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const dbConnection = require('./config/db');
 
 const app = express();
-const PORT = 5001;
-const whitelist = ['http://localhost:3000'];
+const PORT = process.env.PORT || 3000;
+// const whitelist = ['http://localhost:3000'];
 
-const corsOptions = {
-  origin(origin, callback) {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//   origin(origin, callback) {
+//     if (!origin || whitelist.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   credentials: true,
+// };
+// app.use(cors(corsOptions));
+
 app.use(express.json());
 
 dbConnection();
