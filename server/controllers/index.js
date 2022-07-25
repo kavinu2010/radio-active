a0bbb31562e6a7e217cfeef9ce9386ed8e8c160c
salt-dev <salt-dev@salt-devs-MacBook-Pro-2.local>
2022-07-25 13:50:16 +0200

    updating api uri

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,8 +3,7 @@ const User = require('../models/User');
 
 exports.getStations = async (req, res) => {
   const { country } = req.params; // set up on frontend request
-  const uri = `http://91.132.145.114/json/stations/bycountry/${country}?hidebroken=true&order=name&limit=10`;
-  // https://de1.api.radio-browser.info/json/stations/bycountry/austria
+  const uri = `https://de1.api.radio-browser.info/json/stations/bycountry/${country}?hidebroken=true&order=name&limit=10`;
   try {
     const data = await fetch(uri).then(result => result.json());
     const filteredStations = await data.map(station => (
